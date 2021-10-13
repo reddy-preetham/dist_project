07f6bc42f43fa65e417dcf0228399f317f9a8ae9
preetham212 <62885583+preetham212@users.noreply.github.com>
2021-10-12 21:29:37 -0400

    crypto utils

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,4 +1,9 @@
-def hash(vote_info):
-    return vote_info
-def sign(record,name):
-    return hash((record,name))
+import nacl.encoding
+import nacl.hash
+from nacl.signing import SigningKey
+from replica import *
+HASHER = nacl.hash.sha256
+def hash(msg):
+    return HASHER(msg, encoder=nacl.encoding.HexEncoder)
+def sign(record):
+    return private_key.sign(hash(record))
--- replica.py
+++ replica.py
@@ -1,6 +1,9 @@
 window_size=5
 num_validators=10
 name=""
+public_keys=[]
+private_key=""
+f=3
 class Replica:
     def getName():
         return name
