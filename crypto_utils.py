06e2fc949efad69436e7d2be594119f94f0bb3ca
preetham212 <62885583+preetham212@users.noreply.github.com>
2021-10-14 18:12:54 -0400

    replica

--- crypto_utils.py
+++ crypto_utils.py
@@ -3,6 +3,7 @@ import nacl.hash
 from nacl.signing import SigningKey
 from nacl.signing import VerifyKey
 from nacl.encoding import HexEncoder
+from replica import *
 
 import pickle
 # from replica import Replica
@@ -10,7 +11,7 @@ HASHER = nacl.hash.sha256
 def hash(msg):
     return HASHER(pickle.dumps(msg))
 
-def sign(record,key):
+def sign(record,key=Replica.private_key):
     return key.sign(pickle.dumps(record),encoder=HexEncoder)
 
 def verify_decode(record,key):
