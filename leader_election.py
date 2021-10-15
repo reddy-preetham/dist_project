deaccf002b1fb244480c2b6d7c1ae65d87685fcd
sathwikyeluri <sathwikyeluri@gmail.com>
2021-10-14 22:01:09 -0400

    leader election changed from self to cls

--- leader_election.py
+++ leader_election.py
@@ -1,41 +1,44 @@
 from pacemaker import Pacemaker
+from utils import *
 import math
 import random
 
 class LeaderElection:
-    def __init__(self): #need to know constructors
-        self.validators= list()
-        self.window_size = 0    #need to initialize 
-        self.exclude_size = 0   #need to initialize 
-        self.reputation_leaders = dict()
+    validators= list()
+    window_size = Config.window_size   
+    exclude_size = Config.exclude_size
+    reputation_leaders = dict()
     
-    def elect_reputation_leader(self, qc):
+    @classmethod
+    def elect_reputation_leader(cls, qc):
         active_validators = set()
         last_authors = set()
         current_qc = qc
         curr_window_size=0
-        while(curr_window_size<self.window_size or len(last_authors)<self.exclude_size):
+        while(curr_window_size<cls.window_size or len(last_authors)<cls.exclude_size):
             current_block = Ledger.committed_block(current_qc.vote_info.parent_id)
             block_author = current_block.author
-            if curr_window_size<self.window_size:
+            if curr_window_size<cls.window_size:
                 active_validators = set.union(active_validators, current_qc.signatures.signers())
-            if(len(last_authors) < self.exclude_size):
+            if(len(last_authors) < cls.exclude_size):
                 last_authors.add(block_author)
             current_qc = current_block.qc
             curr_window_size+=1
         active_validators = active_validators-last_authors
         random.seed(qc.vote_info.round)
         return random.choice(active_validators)
-        
-    def update_leaders(self, qc):
+
+    @classmethod    
+    def update_leaders(cls, qc):
         extended_round = qc.vote_info.parent_round
         qc_round = qc.vote_info.round
         current_round = Pacemaker.current_round
         if extended_round+1 == qc_round and qc_round+1==current_round:
-            self.reputation_leaders[current_round+1] = self.elect_repuation_leader(qc)
+            cls.reputation_leaders[current_round+1] = cls.elect_repuation_leader(qc)
     
-    def get_leader(self, round):
-        if round in self.reputation_leaders:
-            return self.reputation_leaders[round]
-        return self.validators[math.floor(round/2)%len(self.validators)]
+    @classmethod
+    def get_leader(cls, round):
+        if round in cls.reputation_leaders:
+            return cls.reputation_leaders[round]
+        return cls.validators[math.floor(round/2)%len(cls.validators)]
         
