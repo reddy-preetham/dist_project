2dc6603fb4b9e80c1006db27f7bcd21053f6b19c
preetham212 <62885583+preetham212@users.noreply.github.com>
2021-10-14 21:54:28 -0400

    refactor

--- block_tree.py
+++ block_tree.py
@@ -1,8 +1,6 @@
 # from timeout import TimeoutInfo
 from ledger import Ledger
-from crypto_utils import *
-from replica import *
-import crypto_utils
+from utils import *
 from block import *
 
 class VoteInfo:
@@ -26,20 +24,20 @@ class VoteMsg:
         self.vote_info = vote_info
         self.ledger_commit_info= ledger_commit_info
         self.high_commit_qc = high_commit_qc
-        self.sender = name
-        self.signature = sign(self.ledger_commit_info.to_string())
+        self.sender = Config.replica_id
+        self.signature = sign(self.ledger_commit_info,Config.private_key)
 
 class QC:
     def __init__(self, vote_info, ledger_commit_info, signatures): #need to update object variables
         self.vote_info = vote_info
         self.ledger_commit_info = ledger_commit_info
         self.signatures = signatures
-        self.author = name
-        self.author_signature = sign("".join(self.signatures))
+        self.author = Config.replica_id
+        self.author_signature = sign("".join(self.signatures),Config.private_key)
 
 
-    def to_string(self):
-        return hash("".join(self.vote_info).join(self.ledger_commit_info.to_string()).join(self.signatures).join(self.author).join(self.author_signature))
+    # def to_string(self):
+    #     return hash("".join(self.vote_info).join(self.ledger_commit_info.to_string()).join(self.signatures).join(self.author).join(self.author_signature))
 
 
 
@@ -56,8 +54,8 @@ class BlockTree:
     high_qc=0
     high_commit_qc=0 
     pending_votes={}
-    pending_block_tree= Tree() 
-    genesis_block=Block(name, -1, [], -1, hash("".join(name).join(-1).join([]).join(-1).join([])))
+    genesis_block=Block(Config.replica_id, -1, [], -1, hash("".join(Config.replica_id).join(-1).join([]).join(-1).join([])))
+    pending_block_tree= Tree(genesis_block) 
 
 
     def __init__(self):
@@ -67,25 +65,25 @@ class BlockTree:
         if qc.ledger_commit_info.commit_state_id != None:
             Ledger.commit(cls.pending_block_tree.get_block(qc.vote_info.parent_id))
             cls.pending_block_tree.prune(qc.vote_info.parent_id) #need to implement it
-            cls.high_commit_qc = max(qc, cls.high_commit_qc)
-        cls.high_qc = max(qc, cls.high_qc)
+            cls.high_commit_qc = max(qc.vote_info.round, cls.high_commit_qc.vote_info.round)
+        cls.high_qc = max(qc.vote_info.round, cls.high_qc.vote_info.round)
 
     def execute_and_insert(cls, b):
         Ledger.speculate(b.qc.block_id,b)
-        cls.pending_block_tree.add(b) #need to implement it
+        cls.pending_block_tree.add(b) 
 
     def process_vote(cls, v):
         cls.process_qc(v.high_commit_qc)
-        vote_idx = hash(v.ledger_commit_info.to_string())
+        vote_idx = hash(v.ledger_commit_info)
         cls.pending_votes.setdefault(vote_idx,set())
         cls.pending_votes[vote_idx].add(v.signature)
-        if len(cls.pending_votes[vote_idx])==2*f+1:
-            qc = QC(v.vote_info, v.state_id, cls.pending_votes[vote_idx])
+        if len(cls.pending_votes[vote_idx])==2*Config.n_faulty_replicas+1:
+            qc = QC(v.vote_info, v.state_id, list(cls.pending_votes[vote_idx]))
             return qc
         return None
 
     def generate_blocks(cls,txns, current_round):
-        return Block(name, current_round, txns, cls.high_qc, hash("".join(name).join(current_round).join(txns).join(cls.high_qc.vote_info.id).join(cls.high_qc.signatures))) # need to implement it
+        return Block(Config.replica_id, current_round, txns, cls.high_qc, hash("".join(Config.replica_id).join(current_round).join(txns).join(cls.high_qc.vote_info.id).join(cls.high_qc.signatures))) # need to implement it
 
 
         
