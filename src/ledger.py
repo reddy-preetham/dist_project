d2ea7cdcdc7f777b20f89c96a5152fb131959dc8
preetham212 <62885583+preetham212@users.noreply.github.com>
2021-10-23 20:47:15 -0400

    submitted version

---
+++ src/ledger.py
@@ -0,0 +1,73 @@
+from utils import *
+import os
+from tree import Tree
+from cacheout import Cache
+import logging
+
+
+class LedgerBlock:
+    """Ledger block saved in pending ledger state tree
+    Consists of state id and payload of transactions.
+    """
+
+    def __init__(self, state_id, payload):
+        self.id = state_id
+        self.payload = payload
+
+
+class Ledger:
+    """Ledger class and functions analogous to pseudocode"""
+    # Genesis ledger block which is added to pending ledger tree when created
+    genesis_block = LedgerBlock("genesis_state_id", ["genesis transaction"])
+    pending_ledger_tree = Tree(genesis_block)
+    # block state map to store mappings from block id to state id
+    block_state_map = {}
+
+    @classmethod
+    def initialize_ledger(cls):
+        """Initialize ledger state invoked from Replica"""
+        # Cache of commited blocks to return block with block id if commited
+        # Cache is set with max size as described in phase 2 document
+        cls.commited_blocks = Cache(maxsize=max(
+            Config.window_size, 2*Config.n_replicas))
+        cls.ledger_name = "../ledgers/"+str(Config.replica_id)+"_ledger"
+        cls.block_state_map["genesis_id"] = "genesis_state_id"
+
+    @classmethod
+    def speculate(cls, prev_block_id, block):
+        """Speculates"""
+        parent_state_id = cls.block_state_map.get(
+            prev_block_id, "genesis_state_id")
+        state_id = hash((parent_state_id, block.payload))
+
+        blk = LedgerBlock(state_id, block.payload)
+        cls.pending_ledger_tree.add(blk, parent_state_id)
+
+        cls.block_state_map[block.id] = state_id
+
+    @classmethod
+    def pending_state(cls, block_id):
+        return cls.block_state_map.get(block_id, None)
+
+    @classmethod
+    def commit(cls, block):
+        # block=cls.pending_ledger_tree.get_block(cls.block_state_map[block_id])
+        if block.id not in cls.commited_blocks:
+            # print(block.round,block.payload)
+            logging.info("Ledger : commited block txn = {}, round = {}, author = {}".
+                         format(block.payload, block.round, block.author))
+            cls.persist(cls.block_state_map[block.id])
+            cls.pending_ledger_tree.prune(cls.block_state_map[block.id])
+            cls.commited_blocks.set(block.id, block)
+
+    @classmethod
+    def committed_block(cls, block_id):
+        cls.commited_blocks.get(block_id, None)
+
+    @classmethod
+    def persist(cls, state_id):
+        with open(cls.ledger_name+".txt", "a") as myfile:
+            myfile.write(
+                "\n".join(cls.pending_ledger_tree.get_block(state_id).payload)+"\n")
+            myfile.flush()
+            os.fsync(myfile.fileno())
---
+++ src/utils.py
@@ -0,0 +1,66 @@
+import nacl.encoding
+import nacl.hash
+from nacl.signing import SigningKey
+from nacl.signing import VerifyKey
+from nacl.encoding import HexEncoder
+import pickle
+
+HASHER = nacl.hash.sha256
+
+
+class Config:
+    """Config class, variables are set from Replica and used
+    by functions in utils and ledger modules.
+    """
+    replica_id = ""
+    n_replicas = 0
+    window_size = 0
+    private_key = SigningKey.generate()
+    replica_pub_keys = []
+
+
+def hash(msg):
+    """Returns string representation hash of object msg."""
+    return (HASHER(pickle.dumps(msg))).decode("utf-8")
+
+
+def sign_record(record, key):
+    """Signs and returns signature of a record with a private key."""
+    return key.sign(message=pickle.dumps(record), encoder=HexEncoder)
+
+
+def verify_signature(signature, key):
+    """Verifies signature with a public key"""
+    verify_key = VerifyKey(key, encoder=HexEncoder)
+    try:
+        verify_key.verify(signature, encoder=HexEncoder)
+    except nacl.exceptions.BadSignatureError:
+        return False
+    return True
+
+
+def validate_signatures(signatures_list):
+    """Validates signatures in signature list with replica public keys
+    and returns True if the signature can be vaidated by
+    any of the replica public key.
+    """
+    for signature in signatures_list:
+        is_valid = False
+        for key in Config.replica_pub_keys.values():
+            is_valid = is_valid or verify_signature(signature, key)
+        if not is_valid:
+            return False
+    return True
+
+
+def get_signers(signatures_list):
+    """Returns set of signers (replica ids) of signatures
+    in the signature list.
+    """
+    signers = set()
+    for signature in signatures_list:
+        for key, value in Config.replica_pub_keys:
+            if verify_signature(signature, value):
+                signers.add(key)
+                break
+    return signers
